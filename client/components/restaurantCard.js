820e08e2c8f98980d1067f797fb7fd53563b3fc5
tayoolaigbe <tayoolaigbe@outlook.com>
2023-06-09 00:30:38 +0100

    Completed the RestaurantScreen

--- client/components/restaurantCard.js
+++ client/components/restaurantCard.js
@@ -2,30 +2,36 @@ import { View, Text, TouchableWithoutFeedback, Image } from 'react-native';
 import React from 'react';
 import * as Icon from 'react-native-feather';
 import { themeColors } from '../theme';
+import { useNavigation } from '@react-navigation/native';
 
 export default function RestaurantCard({ item }) {
+	const navigation = useNavigation();
 	return (
-		<TouchableWithoutFeedback>
+		<TouchableWithoutFeedback
+			onPress={() => navigation.navigate('Restaurant', { ...item })}
+		>
 			<View
-				style={{ shadowColor: themeColors.bgColor(0.2), shadowRadius: 7 }}
-				className="mr-6 bg-white rounded-3xl shadow-lg"
+				style={{ shadowColor: themeColors.bgColor(0.9), shadowRadius: 7 }}
+				className="mr-6 bg-white rounded-3xl shadow-lg mb-2"
 			>
 				<Image className="h-36 w-64 rounded-t-3xl" source={item.image} />
 
 				<View className="px-3 pb-4 space-y-2">
-					<Text className="text-lg font-bold pt-2">{item.title}</Text>
+					<Text className="text-lg font-bold pt-2">{item.name}</Text>
 					<View className="flex-row items-center space-x-1">
 						<Image
 							source={require('../assets/images/fullStar.png')}
 							className="h-4 w-4"
 						/>
 						<Text className="text-xs">
-							<Text className="text-green-700">{item.rating}</Text>
+							<Text className="text-green-700">{item.stars}</Text>
 							<Text className="text-gray-700">
 								{' '}
 								({item.reviews} review)
 							</Text> ·{' '}
-							<Text className="font-semibold text-gray-700">{item.type}</Text>
+							<Text className="font-semibold text-gray-700">
+								{item.category}
+							</Text>
 						</Text>
 					</View>
 					<View className="flex-row items-center space-x-1">
