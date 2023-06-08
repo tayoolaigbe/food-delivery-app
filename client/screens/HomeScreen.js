8ad5b4a34cf0327211578e77481a3c367972f2e6
tayoolaigbe <tayoolaigbe@outlook.com>
2023-06-08 21:53:47 +0100

    Completed the SearchBar in the HomeScreen

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -1,11 +1,36 @@
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
+import * as Icon from 'react-native-feather';
+import { themeColors } from '../theme';
 
 export default function HomeScreen() {
 	return (
-		<SafeAreaView>
-			<Text>HomeScreen</Text>
+		<SafeAreaView className="bg-white">
+			<StatusBar barStyle="darkContent" />
+			{/* search bar */}
+			<View className="flex-row items-center space-x-2 px-4 pb-2">
+				<View className="flex-row flex-1 items-center p-3 rounded-full border border-gray-300">
+					<Icon.Search height="25" width="25" stroke="gray" />
+					<TextInput placeholder="Restaurants" className="ml-2 flex-1" />
+					<View className="flex-row items-center space-x-1 border-0 border-l-2 pl-2 border-l-gray-300">
+						<Icon.MapPin height="20" width="20" stroke="gray" />
+						<Text className="text-gray-600">Edinburgh, UK</Text>
+					</View>
+				</View>
+				<View
+					style={{ backgroundColor: themeColors.bgColor(1) }}
+					className="p-3 rounded-full"
+				>
+					<Icon.Sliders
+						height="25"
+						width="25"
+						strokeWidth="2.5"
+						stroke="white"
+					/>
+				</View>
+			</View>
 		</SafeAreaView>
 	);
 }
