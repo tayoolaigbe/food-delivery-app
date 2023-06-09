c40532432cb2c719ed6e34c6f527ec0d4587bf72
tayoolaigbe <tayoolaigbe@outlook.com>
2023-06-09 08:38:59 +0100

    Adding the reducers and slices to the application

--- client/App.js
+++ client/App.js
@@ -1,7 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigation from './navigation';
+import { Provider } from 'react-redux';
 
 export default function App() {
-	return <Navigation />;
+	return;
+	<Provider store={store}>
+		<Navigation />
+	</Provider>;
 }
